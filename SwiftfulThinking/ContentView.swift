6520aabe3726adfe54dfc09f9fe97dc2b020bbca
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-08-20 01:30:46 +0600

    Initial Commit

---
+++ SwiftfulThinking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftfulThinking
+//
+//  Created by A.f. Adib on 8/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulThinking/SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingApp.swift
+//  SwiftfulThinking
+//
+//  Created by A.f. Adib on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
