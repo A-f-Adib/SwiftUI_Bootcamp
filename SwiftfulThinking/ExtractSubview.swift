f8aae07a506e3761c23f43894e0b8f4eb3075b3b
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-08-25 05:19:18 +0600

    commit-2

---
+++ SwiftfulThinking/ExtractSubview.swift
@@ -0,0 +1,51 @@
+//
+//  ExtractSubview.swift
+//  SwiftfulThinking
+//
+//  Created by A.f. Adib on 8/25/23.
+//
+
+import SwiftUI
+
+struct ExtractSubview: View {
+    var body: some View {
+        
+        ZStack{
+            Color.green
+                .ignoresSafeArea()
+            
+            HStack{
+            ExtractedView(Title: "Apples", Count: 10, color: .red)
+            ExtractedView(Title: "Oranges", Count: 20, color: .orange)
+            ExtractedView(Title: "Lichis", Count: 30, color: .pink)
+          }
+        }
+    }
+}
+
+struct ExtractSubview_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractSubview()
+    }
+}
+
+struct ExtractedView: View {
+    
+    
+    let Title : String
+    let Count : Int
+    let color : Color
+    
+    var body: some View {
+        VStack(spacing :0){
+            
+            Text("\(Count)")
+                .padding()
+            Text(Title)
+                .padding(5)
+                .foregroundColor(Color.white)
+        }       .font(.largeTitle)
+            .background(color)
+            .cornerRadius(15)
+    }
+}
---
+++ SwiftfulThinking/TernaryOperator.swift
@@ -0,0 +1,20 @@
+//
+//  TernaryOperator.swift
+//  SwiftfulThinking
+//
+//  Created by A.f. Adib on 8/25/23.
+//
+
+import SwiftUI
+
+struct TernaryOperator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TernaryOperator_Previews: PreviewProvider {
+    static var previews: some View {
+        TernaryOperator()
+    }
+}
